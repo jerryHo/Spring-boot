553d948c80dcd7d36166bb35b7289be0ceaa59a6
jerryHo <chungjerry94@gmail.com>
2025-12-01 16:14:51 +0800

    Added project files with adjusted line endings

---
+++ src/main/java/com/example/demotwo/DemotwoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/qq")
+public class DemotwoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemotwoApplication.class, args);
+  }
+
+  @GetMapping("/x")
+  public String hello() {
+    return "Hey, Spring Boot 的 Hello World !";
+  }
+
+  @GetMapping("/indexx")
+  public String helloIndex() {
+    return "index";
+  }
+}
---
+++ src/main/java/com/example/demotwo/TestEntityController.java
@@ -0,0 +1,28 @@
+package com.example.demotwo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestEntityController {
+
+    @Autowired
+    private TestEntityRepository repository;
+
+    // Endpoint to create a new TestEntity
+    @PostMapping("/add")
+    public TestEntity addTestEntity(@RequestBody TestEntity testEntity) {
+        return repository.save(testEntity);
+    }
+
+    // Endpoint to retrieve all TestEntities
+    @GetMapping("/all")
+    public List<TestEntity> getAllTestEntities() {
+        return repository.findAll();
+    }
+
+    
+}
