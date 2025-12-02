53f5f836f4d6bafdaad529b624a53e668a3d5050
jerryHo <chungjerry94@gmail.com>
2025-12-02 18:03:08 +0800

    Redirect root path / to index.html

--- src/main/java/com/example/demotwo/DemotwoApplication.java
+++ src/main/java/com/example/demotwo/DemotwoApplication.java
@@ -2,26 +2,12 @@ package com.example.demotwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+// Remove @RestController and @RequestMapping from here
 @SpringBootApplication
-@RestController
-@RequestMapping("/")
 public class DemotwoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemotwoApplication.class, args);
-  }
-
-  @GetMapping("/")
-  public String hello() {
-    return "Hey, Spring Boot 的 Hello World !";
-  }
-
-  @GetMapping("/indexx")
-  public String helloIndex() {
-    return "index";
   }
 }
